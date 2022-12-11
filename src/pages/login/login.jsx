fbe64c2630ccf917524647d941ea12358b25a7b4
zhenxxx7 <jordy.zeventh@gmail.com>
2022-12-11 21:30:09 +0700

    first commit and add login pages

---
+++ src/pages/login/login.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function Login() {
+    return (
+        <div>
+            <div className="text-center md: mt-20 mx-auto:text-justify px-8 text-4xl font-semibold">
+                <h1>WELCOME TO UNKLAB FORMS</h1>
+            </div>
+            <div>
+                <img src=".\assets\login-img1.png" className="" alt="illustration" />
+            </div>
+            <div className="flex flex-col items-center justify-center">
+                <button className="bg-orange-400 hover:bg-orange-600 text-white font-bold py-2 px-4 rounded-md">
+                    Login Here
+                </button>
+                <h3 className="text-center md: mt-2 mx-auto:text-justify px-8 text-1xl font-medium">
+                    Login with your Unklab account
+                </h3>
+            </div>
+        </div>
+    )
+}
