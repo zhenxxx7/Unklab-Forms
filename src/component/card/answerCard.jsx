e2b8d457c35024ce8877f99a68aa19e8c9e55ba5
zhenxxx7 <jordy.zeventh@gmail.com>
2022-12-18 21:49:39 +0700

    answer card

--- src/component/card/answerCard.jsx
+++ src/component/card/answerCard.jsx
@@ -1,6 +1,39 @@
 import React from "react";
 
 export default function AnswerCard({ id, img}) {
+
+const [option, /*setOption*/] = React.useState([
+        {
+            id: 1,
+            value: "A",
+            text: "Jawaban A",
+            isCorrect: false,
+        },
+        {
+            id: 2,
+            value: "B",
+            text: "Jawaban B",
+            isCorrect: false,
+        },
+        {
+            id: 3,
+            value: "C",
+            text: "Jawaban C",
+            isCorrect: false,
+        },
+
+    ]);
+
+    React.useEffect(() => {
+        if (img) {
+            const previewImage = document.getElementById("previewImage");
+            const imgElement = document.createElement("img");
+            imgElement.src = img;
+            imgElement.className = "w-64 h-64 object-cover rounded-lg";
+            previewImage.appendChild(imgElement);
+        }
+    }, [img]);
+
     
 
     return (
@@ -14,13 +47,20 @@ export default function AnswerCard({ id, img}) {
                 <div id="previewImage" className="relative mt-4 flex flex-col items-center ">
                     
                 </div>
-
-                <div className="flex flex-col items-center justify-center mt-4">
-                    <div className="flex flex-row items-center justify-center">
-
-
+                
+                {option.map((item) => (
+                    <div className="flex flex-row items-center justify-start mt-4">
+                        <div className="flex flex-col items-center justify-center">
+                            <input type="radio" id={item.value} name="option" value={item.value} />
+                        </div>
+                        <div className="flex flex-col items-center justify-center">
+                            <p className="block w-full font-medium text-lg text-black focus:outline-none">
+                                {item.text}
+                            </p>
+                        </div>
                     </div>
-                </div>
+                ))}
+
             </div>
         </div>
     );
